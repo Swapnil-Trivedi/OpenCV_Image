6810f92661894399e0e5143f4cee4656d6e4849e
Swapnil_Scage <63580574+Swapnil-Trivedi@users.noreply.github.com>
2020-08-03 12:00:04 +0530

    Add files via upload

---
+++ FOURTH_BLENDING_IMAGES/main.cpp
@@ -0,0 +1,25 @@
+#include <opencv2/opencv.hpp>
+using namespace cv;
+
+int main(int argc, char *argv[])
+{
+    //thhis is an intresting thing to work with
+    //let's load our images first
+    Mat blue=imread("blue.png",1);
+    Mat yellow=imread("yellow.png",1);
+    //we wil be combining these two images to form a circle of green color
+    //Blending is nothing but adding two images together
+    Mat result;
+    const float alpha=0.5;
+    //we make alpha a constant
+    float beta=(1-alpha);
+    float gamma=0.0;
+    addWeighted(blue,alpha,yellow,beta,gamma,result);
+    imshow("Blended Image",result);
+    imwrite("result_image.png",result);
+    waitKey(0);
+    return 0;
+}
+
+
+
